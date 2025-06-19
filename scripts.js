0c0c07e5d9275ccc2daeb66012f98531ff2b0320
Daniel <83424429+Daaniel-Dev@users.noreply.github.com>
2025-06-19 18:08:13 -0300

    Adicionando arquivos

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function mostrarAlerta() {
+    alert("Bem vindo à nossa feira de livros!");
+}
+
+function mostrarLivros() {
+    var livros = "Dom Quixote - Miguel Cervantes\nCem Anos de Solidão - Gabriel García Márquez\n1984 - George Orwell\nVidas Secas - Graciliano Ramos"
+    alert(livros);
+}
